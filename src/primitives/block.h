45809bd1ba3b66d79342ebf5eeed5163861cc4cb
effectsToCause <dpike@vericoin.info>
2017-06-21 22:06:13 -0400

    VeriCoin block version

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -21,7 +21,7 @@ class CBlockHeader
 {
 public:
     // header
-    static const int32_t CURRENT_VERSION=7;
+    static const int32_t CURRENT_VERSION=6;
     int32_t nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
